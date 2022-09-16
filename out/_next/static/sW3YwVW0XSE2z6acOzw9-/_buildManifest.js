0510798cce7e16e32984eccf7141b4f70390c6ba
butcher73 <butcher73@getethpow.clock>
2022-09-16 19:20:51 +0700

    add missing files

---
+++ out/_next/static/sW3YwVW0XSE2z6acOzw9-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fd38643b728984398e762.css","static\u002Fchunks\u002Fpages\u002Findex-6cda82658ec943ee42e8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-12be42e2bd7c1cc4c354.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
